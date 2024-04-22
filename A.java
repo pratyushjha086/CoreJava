c3a85d24d934d2758ee43c9ba0960410f0cf672e
Pratyush Kumar <90020920+pratyushjha086@users.noreply.github.com>
2024-04-22 12:38:33 +0530

    Update A.java

--- A.java
+++ A.java
@@ -3,12 +3,12 @@ public class A {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Double a,b,c;
+		Double z,b,c;
 		System.out.print("Enter any two number: ");
 		Scanner S = new Scanner (System.in);
 		a = S.nextDouble();
 		b = S.nextDouble();
-        c = a+b;
+        c = z+b;
         System.out.print("Sum of two number:" + c);
 
 	}
